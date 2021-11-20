3d8b8d2b55775867b498706308b2fc67d2ced9b9
Xincheng Chi <xincheng.chi@stonybrook.edu>
2021-11-20 14:12:24 -0500

    first commit

---
+++ client/src/components/EditToolbar.js
@@ -0,0 +1,84 @@
+import { useContext } from 'react'
+import { GlobalStoreContext } from '../store'
+import Button from '@mui/material/Button';
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
+import CloseIcon from '@mui/icons-material/HighlightOff';
+
+/*
+    This toolbar is a functional React component that
+    manages the undo/redo/close buttons.
+    
+    @author McKilla Gorilla
+*/
+function EditToolbar() {
+    const { store } = useContext(GlobalStoreContext);
+
+    function handleUndo() {
+        store.undo();
+    }
+    function handleRedo() {
+        store.redo();
+    }
+    function handleClose() {
+        store.closeCurrentList();
+    }
+    function haveUndo(){
+        return store.canUndo();
+    }
+    function haveRedo(){
+        return store.canRedo();
+    }
+    let editStatus = false;
+    if (store.isListNameEditActive) {
+        editStatus = true;
+    }
+    let enabledButtonClass = "top5-button";
+    if(store.isItemEditActive){
+        enabledButtonClass = "top5-button-disabled";
+    }  
+    return (
+        <div id="edit-toolbar">
+            {haveUndo() ?
+            <Button 
+                id='undo-button'
+                onClick={handleUndo}
+                variant="contained"
+                className = {enabledButtonClass}>
+                    <UndoIcon />
+            </Button> : 
+            <Button 
+                id='undo-button'
+                variant="contained"
+                className = "top5-button-disabled">
+                <UndoIcon />
+            </Button> 
+            }
+            {haveRedo() ? 
+            <Button 
+                id='redo-button'
+                onClick={handleRedo}
+                variant="contained"
+                className = {enabledButtonClass}>
+                    <RedoIcon />
+            </Button> : 
+            <Button 
+                id='redo-button'
+                variant="contained"
+                className = "top5-button-disabled">
+                <RedoIcon />
+            </Button> 
+            }
+            <Button 
+                disabled={editStatus}
+                id='close-button'
+                onClick={handleClose}
+                className = {enabledButtonClass}
+                variant="contained">
+                    <CloseIcon />
+            </Button>
+        </div>
+    )
+}
+
+export default EditToolbar;
---
+++ client/src/components/Top5Item.js
@@ -0,0 +1,141 @@
+import { React, useContext, useState } from "react";
+import { GlobalStoreContext } from '../store'
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+/*
+    This React component represents a single item in our
+    Top 5 List, which can be edited or moved around.
+    
+    @author McKilla Gorilla
+*/
+function Top5Item(props) {
+    const { store } = useContext(GlobalStoreContext);
+    const [editActive, setEditActive] = useState(false);
+    const [draggedTo, setDraggedTo] = useState(0);
+    const [text, setText] = useState(props.text);
+
+    function handleDragStart(event, targetId) {
+        event.dataTransfer.setData("item", targetId);
+    }
+
+    function handleDragOver(event) {
+        event.preventDefault();
+    }
+
+    function handleDragEnter(event) {
+        event.preventDefault();
+        setDraggedTo(true);
+    }
+
+    function handleDragLeave(event) {
+        event.preventDefault();
+        setDraggedTo(false);
+    }
+
+    function handleDrop(event, targetId) {
+        event.preventDefault();
+        let sourceId = event.dataTransfer.getData("item");
+        sourceId = sourceId.substring(sourceId.indexOf("-") + 1);
+        setDraggedTo(false);
+        // UPDATE THE LIST
+        store.addMoveItemTransaction(sourceId, targetId);
+    }
+
+    function handleToggleEdit(event){
+        event.stopPropagation();
+        toggleEdit();
+    }
+
+    function toggleEdit() {
+        let newActive = !editActive;
+        if (newActive) {
+            store.setIsItemEditActive();
+        }
+        setEditActive(newActive);
+    }
+
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            store.addUpdateItemTransaction(index,event.target.value);
+            toggleEdit();
+        }
+        handleUpdateText(event);
+    }
+
+    function handleUpdateText(event) {
+        setText(event.target.value);
+    }
+
+    function handleBlur(event){
+        store.addUpdateItemTransaction(index,event.target.value);
+        toggleEdit();
+        handleUpdateText(event);
+    }
+
+    let { index} = props;
+
+    let itemClass = "top5-item";
+
+    if (draggedTo) {
+        itemClass = "top5-item-dragged-to";
+    }
+    
+    let itemelement = 
+            <ListItem
+            id={'item-' + (index+1)}
+            className={itemClass}
+            onDragStart={(event) => {
+                handleDragStart(event, (index+1))
+            }}
+            onDragOver={(event) => {
+                handleDragOver(event, (index+1))
+            }}
+            onDragEnter={(event) => {
+                handleDragEnter(event, (index+1))
+            }}
+            onDragLeave={(event) => {
+                handleDragLeave(event, (index+1))
+            }}
+            onDrop={(event) => {
+                handleDrop(event, (index+1))
+            }}
+            draggable="true"
+            sx={{ display: 'flex', p: 1 }}
+            style={{
+                fontSize: '48pt',
+                width: '100%'
+            }}
+        >
+        <Box sx={{ p: 1 }}>
+            <IconButton onClick={handleToggleEdit} aria-label='edit'>
+                <EditIcon style={{fontSize:'48pt'}}  />
+            </IconButton>
+        </Box>
+            <Box sx={{ p: 1, flexGrow: 1 }}>{text}</Box>
+        </ListItem>
+    if (editActive) {
+        itemelement =
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id={'item-' + (index+1)}
+                label="ItemName"
+                className='itemClass'
+                onKeyPress={handleKeyPress}
+                onBlur={handleBlur}
+                defaultValue={text}
+                inputProps={{style: {fontSize: 48}}}
+                InputLabelProps={{style: {fontSize: 24}}}
+                autoFocus
+            />
+    }
+    return (
+        itemelement
+    )
+}
+
+export default Top5Item;
